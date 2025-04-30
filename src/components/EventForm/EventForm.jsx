042c5c2e55648ad41bbc47a0b0bb266ca19a4f69
Afnan Matari <130868488+Afnan112@users.noreply.github.com>
2025-05-01 00:51:10 +0300

    Aded one 'usestate' for all input and implemented handleChange. also added 'handleSubmit' to print all form data in the console

--- src/components/EventForm/EventForm.jsx
+++ src/components/EventForm/EventForm.jsx
@@ -1,41 +1,102 @@
 import React from "react";
 
-function EventForm() {
+function EventForm(props) {
   return (
     <div>
         <h2>Crate your Event</h2>
-        <form>
+        <form onSubmit={props.handleSubmit}>
             <div>
                 <label>Event Title</label>
-                <input id='title' name='title' type="text" required />
+                <input 
+                id='title' 
+                name='title' 
+                type="text" 
+                required 
+                value={props.formInputs.title} 
+                onChange={props.handleChange}
+                />
             </div>
             <div>
                 <label>Start Date</label>
-                <input id='start_date' name='start_date' type="date" required />
+                <input 
+                id='start_date' 
+                name='start_date' 
+                type="date" 
+                required 
+                value={props.formInputs.start_date}
+                onChange={props.handleChange}
+                />
             </div>
             <div>
                 <label>End Date</label>
-                <input id='end_date' name='end_date'  type="date"required />
+                <input 
+                id='end_date' 
+                name='end_date'  
+                type="date"
+                required 
+                value={props.formInputs.end_date}
+                onChange={props.handleChange}
+                />
             </div>
             <div>
                 <label>Start Time</label>
-                <input id='start_time' name='start_time' type="time" required />
+                <input 
+                id='start_time' 
+                name='start_time' 
+                type="time" 
+                required
+                value={props.formInputs.start_time}
+                onChange={props.handleChange}
+                />
             </div>
             <div>
                 <label>End Time</label>
-                <input id='end_time' name='end_time'  type="time" required />
+                <input 
+                id='end_time' 
+                name='end_time'  
+                type="time" 
+                required
+                value={props.formInputs.end_time} 
+                onChange={props.handleChange}
+                />
             </div>
             <div>
                 <label>Location</label>
-                <input id='location' name='location' type="text" required />
+                <input 
+                id='location' 
+                name='location' 
+                type="text" 
+                required 
+                value={props.formInputs.location}
+                onChange={props.handleChange}
+                />
             </div>
             <div>
-                <label>Event Type</label>
-                <input id='event_type' name='event_type' required />
+            <label>Event Type</label>
+            <select 
+            id="event_type" 
+            name="event_type" 
+            required
+            value={props.formInputs.event_type}
+            onChange={props.handleChange}
+            >
+            <option value="workshop">Workshop</option>
+            <option value="course">Course</option>
+            <option value="conference">Conference</option>
+            <option value="seminar">Seminar</option> 
+            {/* Online only */}
+            <option value="webinar">Webinar</option> 
+            <option value="meetup">Meetup</option>
+            </select>
             </div>
             <div>
                 <label>Description</label>
-                <textarea id='description' name='description' required></textarea>
+                <textarea 
+                id='description' 
+                name='description'
+                value={props.formInputs.description}
+                onChange={props.handleChange}
+                ></textarea>
             </div>
 
             <button type="submit">Create Event</button>
--- src/pages/EventAdd.jsx
+++ src/pages/EventAdd.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
+import { useState } from 'react'
 import EventForm from '../components/EventForm/EventForm'
 
 function EventAdd() {
+    /*
+        Because I have many fields, I created a single state,
+        and the "controlled-forms" lesson helped me a lot.
+        In it, we learned how to use a single state for more than one field.
+    */
+    const [formInputs, setFormInputs] = useState({
+        title: '',
+        start_date: '',
+        end_date: '',
+        start_time: '',
+        end_time: '',
+        location: '',
+        event_type: '',
+        description: ''
+    })
+
+    function handleChange(event){
+        // console.log(event.target.name)
+        // console.log(event.target.value)
+        const currentData = {...formInputs} 
+        currentData[event.target.name] = event.target.value
+        setFormInputs(currentData)
+        console.log(formInputs)
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+        console.log('Handle Submit is running')
+        console.log(formInputs)
+    }
+
   return (
     <div>
       <h1>Event Add Page</h1>
-      <EventForm/>
+      <EventForm
+        formInputs={formInputs}
+        setFormInputs={setFormInputs}
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+      />
     </div>
   )
 }
