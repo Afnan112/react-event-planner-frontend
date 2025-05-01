9f1277fd826f41c2193078038deabca56acc4f4c
Afnan Matari <130868488+Afnan112@users.noreply.github.com>
2025-05-01 09:21:55 +0300

    Add API to submit form data to DB

--- src/pages/EventAdd.jsx
+++ src/pages/EventAdd.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import EventForm from '../components/EventForm/EventForm'
+import axios from 'axios'
 
 function EventAdd() {
     /*
@@ -28,10 +29,14 @@ function EventAdd() {
         console.log(formInputs)
     }
 
-    function handleSubmit(event){
+    async function handleSubmit(event){
         event.preventDefault()
         console.log('Handle Submit is running')
         console.log(formInputs)
+        const payload = formInputs
+        const url = 'http://127.0.0.1:8000/api/events/'
+        const response = await axios.post(url, payload)
+        console.log(response)
     }
 
   return (
